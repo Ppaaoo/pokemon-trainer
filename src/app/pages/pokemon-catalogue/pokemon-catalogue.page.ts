faa3eb2afc570e97852a69bc4d562460c6d779dd
Ppaaoo <62310221+Ppaaoo@users.noreply.github.com>
2023-02-03 10:39:42 +0100

    tailwind styling

--- src/app/pages/pokemon-catalogue/pokemon-catalogue.page.ts
+++ src/app/pages/pokemon-catalogue/pokemon-catalogue.page.ts
@@ -30,10 +30,9 @@ export class PokemonCataloguePage implements OnInit {
   }
 
   getPokemon() {
-    this.pokemonCatalogueService.findPokemons(10, this.pokemonCatalogueService.page + 0)
+    this.pokemonCatalogueService.findPokemons(20, this.pokemonCatalogueService.page + 0)
     .subscribe((response: any) => {
       this.pokemonCatalogueService.totalPokemon = response.count;
-      console.log(`${this.pokemonCatalogueService.totalPokemon} and ${this.pokemonCatalogueService.page}`)
       response.results.forEach((result: any) => {
         this.pokemonCatalogueService.getMorePokemonData(result.name)
         .subscribe((uniqueResponse: any) => {
